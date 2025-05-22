80bc5389cd8ca65b72d60798078d0229375fcdfb
Jonathan Martinez <204512686+spinupjmart@users.noreply.github.com>
2025-05-22 19:46:34 -0400

    Comment for removing line highlight

--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -1,10 +1,10 @@
 return {
-    -- kanagawa syntax highlighting
     "rebelot/kanagawa.nvim",
     lazy = false,
     priority = 1000,
     config = function()
         vim.cmd.colorscheme 'kanagawa-dragon'
+        -- remove cursor line highlighting
         vim.cmd([[highlight CursorLine cterm=NONE ctermbg=NONE guibg=NONE]])
     end
 }
