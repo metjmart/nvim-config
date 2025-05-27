a147f39bd93b319accbe556c908516717641c94d
Jonathan Martinez <204512686+spinupjmart@users.noreply.github.com>
2025-05-27 19:44:02 -0400

    Add markdown preview/render; remove italics fonts

--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -3,8 +3,16 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+        require('kanagawa').setup({
+            keywordStyle = { italic = false},
+            overrides = function()
+                return {
+                    ["@variable.builtin"] = { italic = false },
+                }
+            end,
+        })
         vim.cmd.colorscheme 'kanagawa-dragon'
         -- remove cursor line highlighting
         vim.cmd([[highlight CursorLine cterm=NONE ctermbg=NONE guibg=NONE]])
-    end
+    end,
 }
