d60a36d0e4d7941ca07722b2344f78c71b8e8e33
Jonathan Martinez <204512686+spinupjmart@users.noreply.github.com>
2025-06-21 16:28:10 -0400

    Add devicons and buffer source to cmp menu

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -13,6 +13,9 @@ return {
         'hrsh7th/cmp-cmdline',
     },
     {
+        'onsails/lspkind.nvim',
+    },
+    {
         'hrsh7th/nvim-cmp',
         config = function()
             local cmp = require('cmp')
@@ -35,6 +38,28 @@ return {
                     { name = 'buffer' },
                     { name = 'path' },
                 }),
+                formatting = {
+                    format = function(entry, vim_item)
+                        if vim.tbl_contains({ 'path' }, entry.source.name) then
+                            local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
+                            if icon then
+                                vim_item.kind = icon
+                                vim_item.kind_hl_group = hl_group
+                                return vim_item
+                            end
+                        end
+                        return require('lspkind').cmp_format({
+                            mode = "symbol",
+                            menu = ({
+                                buffer = "[Buffer]",
+                                nvim_lsp = "[LSP]",
+                                luasnip = "[LuaSnip]",
+                                nvim_lua = "[Lua]",
+                                latex_symbols = "[Latex]",
+                            }),
+                        })(entry, vim_item)
+                    end
+                }
             })
         end,
     },
