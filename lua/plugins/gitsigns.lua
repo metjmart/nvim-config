b4a2aabc91792c7474cacc0341850d6c065b91f5
Jonathan Martinez <204512686+spinupjmart@users.noreply.github.com>
2025-06-21 16:37:06 -0400

    Add hunk nav fir gitsigns/update keymaps

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -10,7 +10,25 @@ return {
                       opts.buffer = bufnr
                       vim.keymap.set(mode, l, r, opts)
                     end
-                    map('n', '<leader>hp', gitsigns.preview_hunk)
+                    -- navigation
+                    map('n', '<leader>hn', function()
+                        if vim.wo.diff then
+                            vim.cmd.normal({'<leader>hn', bang = true})
+                        else
+                            gitsigns.nav_hunk('next')
+                        end
+                    end
+                    )
+                    map('n', '<leader>hp', function()
+                        if vim.wo.diff then
+                            vim.cmd.normal({'<leader>hp', bang = true})
+                        else
+                            gitsigns.nav_hunk('prev')
+                        end
+                    end
+                    )
+                    -- preview hunks
+                    map('n', '<leader>hh', gitsigns.preview_hunk)
                     map('n', '<leader>hi', gitsigns.preview_hunk_inline)
                 end
             })
